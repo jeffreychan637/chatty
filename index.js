173e3c4ddbe3f90639a59863b468cdcd130662d3
Jeffrey Chan <jeffreychan637@berkeley.edu>
2015-06-13 04:11:57 -0700

    Made user list width smaller so more room for main conversation; added borders

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
 						   
 	var windowHeight = $(window).height();
     var leftover = windowHeight - $(".nav").height();
-    console.log(leftover + "px");
     $(".chats").css("height", leftover.toString() + "px");
-//    $("#chats").css("background-color", "red");
+    $(".user-list").css("height", leftover.toString() + "px");
+    
 	
 });
