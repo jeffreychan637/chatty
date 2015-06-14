1246b7301a03a2fa33393bdf9b861c7c2594ea82
Jeffrey Chan <jeffreychan637@berkeley.edu>
2015-06-13 19:26:09 -0700

    Added text box for responses, designed left nav bar for conversations

--- index.js
+++ index.js
@@ -7,5 +7,16 @@ $(document).ready(function(){
     $(".chats").css("height", leftover.toString() + "px");
     $(".user-list").css("height", leftover.toString() + "px");
     
+    var replyMargin = 2 * parseInt($(".reply").css("margin-left"), 10);
+    var replyPadding = 2 * parseInt($(".reply").css("padding-left"), 10);
+    var replyBorder = 2 * parseInt($(".reply").css("border-width"), 10);
+    var replyWidth = $(".reply-box").width() - replyMargin - replyPadding - replyBorder;
+    $(".reply").css("width", replyWidth.toString() + "px");
+    
+    var remainingHeight = leftover - $(".reply-box").height();
+    console.log(remainingHeight);
+    $(".conversation-box").css("height", remainingHeight.toString() + "px");
+    
+//    $("body").css("max-height", windowHeight.toString() + "px");
 	
 });
