5b3fdec690ab1f3062d353c2d70f3a3bf66c2dfd
Jeffrey Chan <jeffreychan637@berkeley.edu>
2015-06-16 15:13:22 -0700

    Added AngularJS into client side

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 'use strict';
 
-$(document).ready(function(){ 
+angular.module('chatty')
+  .controller('chattyCtrl', function ($scope, $document) {
+    $document.ready(function(){ 
 						   
-	var windowHeight = $(window).height();
-    var leftover = windowHeight - $(".nav").height();
-    $(".chats").css("height", leftover.toString() + "px");
-    $(".user-list").css("height", leftover.toString() + "px");
-    
-    var replyMargin = 2 * parseInt($(".reply").css("margin-left"), 10);
-    var replyPadding = 2 * parseInt($(".reply").css("padding-left"), 10);
-    var replyBorder = 2 * parseInt($(".reply").css("border-width"), 10);
-    var replyWidth = $(".reply-box").width() - replyMargin - replyPadding - replyBorder;
-    $(".reply").css("width", replyWidth.toString() + "px");
-    
-    var remainingHeight = leftover - $(".reply-box").height();
-    console.log(remainingHeight);
-    $(".conversation-box").css("height", remainingHeight.toString() + "px");
+        var windowHeight = $(window).height();
+        var leftover = windowHeight - $(".nav").height();
+        $(".chats").css("height", leftover.toString() + "px");
+        $(".user-list").css("height", leftover.toString() + "px");
+
+        var replyMargin = 2 * parseInt($(".reply").css("margin-left"), 10);
+        var replyPadding = 2 * parseInt($(".reply").css("padding-left"), 10);
+        var replyBorder = 2 * parseInt($(".reply").css("border-width"), 10);
+        var replyWidth = $(".reply-box").width() - replyMargin - replyPadding - replyBorder;
+        $(".reply").css("width", replyWidth.toString() + "px");
+
+        var remainingHeight = leftover - $(".reply-box").height();
+        console.log(remainingHeight);
+        $(".conversation-box").css("height", remainingHeight.toString() + "px");
     
 //    if ($("#y").height() > 21) {
 //        $(""
@@ -23,4 +25,5 @@ $(document).ready(function(){
     
 //    $("body").css("max-height", windowHeight.toString() + "px");
 	
+    });
 });
