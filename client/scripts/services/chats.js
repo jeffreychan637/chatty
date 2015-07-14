aa30fdab3fcdd43f050201bec7753bf1d348c271
Jeffrey Chan <jeffreychan637@berkeley.edu>
2015-07-14 01:36:11 -0700

    Created new chat service to handle conversation processing

---
+++ client/scripts/services/chats.js
@@ -0,0 +1,29 @@
+'use strict';
+/*global $:false, FB:false, jQuery:false */
+
+angular.module('chatty').factory('chats', function () {
+
+  var getRecipient = function(conversation) {
+
+  };
+
+  var getTime = function(time) {
+
+  };
+
+  var getLatest = function(messages) {
+
+  };
+
+  var getUnread = function(messages) {
+
+  };
+
+  var getConversationInfo = function(conversation) {
+
+  };
+
+  return {
+    getConversationInfo: getConversationInfo
+  };
+});
